acca767df5e0e547ba89110e0c131dc51bb58666
dhzdhd <ragas5432100@gmail.com>
2023-12-16 01:12:43 +0530

    Add working decorator impl

--- examples/__main__.py
+++ examples/__main__.py
@@ -1,5 +1,6 @@
 from pysut import Test
 import itertools as it
+import time
 
 
 def key(x):
@@ -10,23 +11,12 @@ def key(x):
 
 
 class Solution:
-    @Test("examples/input1.toml")
     @Test("examples/input2.toml")
-    @Test("examples/input3.toml")
-    def maxArea(self, height: [int], amount: int) -> int:
+    def maxArea(height: [int], amount: int) -> int:
+        time.sleep(1)
         choices = list(it.combinations(enumerate(height), amount))
         m = max(choices, key=key)
 
         t1 = m[0]
         t2 = m[1]
         return abs(t2[0] - t1[0]) * abs(t2[1] - t1[1]) + 1
-
-    # @Test("examples/input.toml")
-    # def a(self) -> int:
-    #     return 1
-
-
-sol = Solution()
-print(sol.maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7], 2))
-print(sol.maxArea([1, 2, 1], 2))
-# print(sol.a())
--- pysut/__main__.py
+++ pysut/__main__.py
@@ -2,42 +2,66 @@ from typing import Callable, Any
 import tomllib as toml
 from pathlib import Path
 from functools import wraps
-from pysut.utils import load_toml, parse
 from dataclasses import dataclass
+import inspect
+from rich.console import Console
+from rich.status import Status
+import time
 
 type Function = Callable[..., Any]
+
+console = Console()
 
 
 @dataclass(frozen=True)
 class _Model:
-    name: str | None
-    input: Any
+    inputs: list[Any]
     output: Any
+    name: str | None = None
+    metadata: str | None = None
 
 
+class ValidationError(Exception):
+    def __init__(self, *args: object) -> None:
+        super().__init__(*args)
+
+
+# Perhaps make a decorator for an entire class with specified method to test
 class Test:
-    called = []
-
-    def __init__(self, file: str | Path) -> None:
-        assert isinstance(file, Path) or isinstance(file, str)
+    def __init__(
+        self,
+        file: str | Path,
+        func: Function | None = None,
+    ) -> None:
+        if not (isinstance(file, Path) or isinstance(file, str)):
+            raise ValueError("File type should be either str or Path")
 
         self._file = file if isinstance(file, Path) else Path(file)
 
-        d = self._load_toml(self._file)
-        self._parse(d)
+        self._data: list[_Model] = []
 
-        self._inputs: list[str] | None = None
-        self._outputs: list[str] = []
+        self._parse(self._load_toml(self._file))
+
+        if func is not None:
+            if inspect.isfunction(func):
+                self._validate(func)
+            else:
+                raise ValueError("func argument is not a function")
 
     def __call__(self, func: Function) -> Any:
+        if inspect.isclass(func):
+            raise ValueError("The decorator cannot be applied on classes")
+        else:
+            if "self" in func.__code__.co_varnames:
+                raise ValidationError(
+                    "The decorator cannot be applied to instance methods"
+                )
+            else:
+                with console.status("[bold green] Running tests...") as status:
+                    self._validate(func)
+
         @wraps(func)
         def wrapper(*args, **kwargs):
-            if id(func) in Test.called:
-                return func(*args, **kwargs)
-
-            Test.called.append(id(func))
-            print(load_toml(self._file))
-
             return func(*args, **kwargs)
 
         return wrapper
@@ -47,4 +71,78 @@ class Test:
             return toml.load(file)
 
     def _parse(self, data: dict[str, Any]):
-        print(data)
+        if not any(["o" in data, "outputs" in data, "data" in data]):
+            raise ValidationError("Must contain one of o, outputs or data keys")
+
+        inputs = []
+        outputs = []
+        metadata = "No metadata"
+        name = "Test case"
+
+        if "cases" in data:
+            ...
+        else:
+            # Use regex to match o+, i+
+            if any(["o" in data, "out" in data, "output" in data, "outputs" in data]):
+                if any(["i" in data, "in" in data, "input" in data, "inputs" in data]):
+                    inputs = data["i"]
+                outputs = data["o"]
+            else:
+                raise ValidationError("No output data given")
+
+            if "metadata" in data:
+                metadata = data["metadata"]
+
+            if "name" in data:
+                name = data["name"]
+
+            if len(inputs) != len(outputs):
+                raise ValidationError(
+                    "Input and output data are not of the same length"
+                )
+
+            for i in range(len(outputs)):
+                self._data.append(
+                    _Model(
+                        inputs=inputs[i],
+                        output=outputs[i],
+                        metadata=metadata,
+                        name=f"{name} [bold blue]{i + 1}[/bold blue]",
+                    )
+                )
+
+    def _validate(self, func: Function):
+        failures = 0
+
+        for index, data in enumerate(self._data):
+            if data.inputs is not None:
+                if not isinstance(data.inputs, list):
+                    raise ValidationError("Inputs must be nested within a list")
+                console.print(f"Input - {data.inputs}")
+                console.print(f"Expected output - {data.output}")
+
+                result = func(*data.inputs)
+                console.print(f"Actual output - {result}")
+
+                if result != data.output:
+                    console.print(
+                        f"\nTask [bold blue]{index + 1}[/bold blue] - [bold red]{data.name} failed\n\n"
+                    )
+                    failures += 1
+                else:
+                    console.print(
+                        f"\nTask [bold blue]{index + 1}[/bold blue] - [bold green]{data.name} complete\n\n"
+                    )
+
+            else:
+                for o in self._outputs:
+                    assert func() == o
+
+        status = (
+            "[bold green]SUCCESS[/bold green]"
+            if failures == 0
+            else "[bold red]FAILURE[/bold red]"
+        )
+        console.print(
+            f"{status} | [bold green]{len(self._data) - failures} passed[/bold green] | [bold red]{failures} failed"
+        )
